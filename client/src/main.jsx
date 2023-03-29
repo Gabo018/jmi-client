d706f9fc2ed93dff2fc0efb52b49e0e2678603d5
Ruzell03 <ruzell@matchmo.ph>
2023-03-29 10:37:10 +0800

    implement react query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from 'react-query'
+import 'antd/dist/antd.css';
 const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
